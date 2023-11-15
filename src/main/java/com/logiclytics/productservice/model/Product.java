52db37e0dcbe9ec0a4c3a59ece297373321ac114
khaledAbdelrehim <khaledabdelrehim0@gmail.com>
2023-11-15 17:25:03 +0100

    refactor(Products): change ID type

--- src/main/java/com/logiclytics/productservice/model/Product.java
+++ src/main/java/com/logiclytics/productservice/model/Product.java
@@ -20,7 +20,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private UUID id;
+    private Long id;
 
     @Column(name = "name")
     private String name;
