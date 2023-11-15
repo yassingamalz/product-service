d3093f61f72368fa68299baa352b312a4dd975d2
yassingamalz <yassen.gamal.yg@gmail.com>
2023-11-15 13:06:38 +0100

    Initial commit

---
+++ src/test/java/com/logiclytics/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logiclytics.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
